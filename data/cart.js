6261736d1e045493e29f3dacb4a9dbcc9f6104b1
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-05-21 16:51:19 +0530

    Saved the cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,20 @@
-export let cart = [
-    {
-        productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity : 1
-    },
-    {
-        productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity : 2
-    }
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+    cart = [
+        {
+            productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity : 1
+        },
+        {
+            productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity : 2
+        }
+    ];
+}
+
+function saveToCart(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addtocart(productId){
     let matcheditem;
@@ -24,6 +31,7 @@ export function addtocart(productId){
             quantity : 1
         });
     }
+    saveToCart();
 }
 
 export function removeItemFromCart(productId){
@@ -34,4 +42,5 @@ export function removeItemFromCart(productId){
         }
     });
     cart = newcart;
+    saveToCart();
 }
