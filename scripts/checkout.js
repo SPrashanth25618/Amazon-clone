77a70fd6a4833ed598fe5457639516ae1d9e153b
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-06-01 10:56:03 +0530

    Added backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,7 @@
 import { rendorOrderSummary } from "./checkout/orderSummary.js";
 import { rendorPaymentSummary } from "./checkout/paymentSummary.js";
+
 // import '../data/cart-class.js';
+import '../data/backend-practice.js';
 rendorOrderSummary();
 rendorPaymentSummary();
