0622750e7eaaeb2dc94ac1f7680988acda9c3ced
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-06-01 11:16:03 +0530

    Load products from backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,77 +1,81 @@
 import {cart,addtocart} from '../data/cart.js';
-import { products } from '../data/products.js';
+import { products,loadProducts } from '../data/products.js';
 import { format_currency } from './utils/money.js';
 
-let productsHtml = '';
-products.forEach((product) => {
-    productsHtml += `
-        <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src=${product.image}>
-          </div>
+loadProducts(rendorProductsGrid);
 
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
-
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="${product.getStarsUrl()}">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
+function rendorProductsGrid(){
+  let productsHtml = '';
+  products.forEach((product) => {
+      productsHtml += `
+          <div class="product-container">
+            <div class="product-image-container">
+              <img class="product-image"
+                src=${product.image}>
             </div>
+
+            <div class="product-name limit-text-to-2-lines">
+              ${product.name}
+            </div>
+
+            <div class="product-rating-container">
+              <img class="product-rating-stars"
+                src="${product.getStarsUrl()}">
+              <div class="product-rating-count link-primary">
+                ${product.rating.count}
+              </div>
+            </div>
+
+            <div class="product-price">
+              ${product.getPrice()}
+            </div>
+
+            <div class="product-quantity-container">
+              <select>
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+
+            ${product.extrainfoHtml()}
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart">
+              <img src="images/icons/checkmark.png">
+              Added
+            </div>
+
+            <button class="add-to-cart-button button-primary js-addtocart" data-product-id = "${product.id}">
+              Add to Cart
+            </button>
           </div>
+      `
+  });
 
-          <div class="product-price">
-            ${product.getPrice()}
-          </div>
+  document.querySelector('.js-products-container').innerHTML = productsHtml;
 
-          <div class="product-quantity-container">
-            <select>
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
+  function updateaddtocart(){
+      let cartquantity = 0;
+      cart.forEach((item) => {
+          cartquantity += item.quantity;
+      });
+      document.querySelector('.js-cartQuantity').innerHTML = cartquantity;
+  }
 
-          ${product.extrainfoHtml()}
-
-          <div class="product-spacer"></div>
-
-          <div class="added-to-cart">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary js-addtocart" data-product-id = "${product.id}">
-            Add to Cart
-          </button>
-        </div>
-    `
-});
-
-document.querySelector('.js-products-container').innerHTML = productsHtml;
-
-function updateaddtocart(){
-    let cartquantity = 0;
-    cart.forEach((item) => {
-        cartquantity += item.quantity;
-    });
-    document.querySelector('.js-cartQuantity').innerHTML = cartquantity;
+  document.querySelectorAll('.js-addtocart').forEach((button) => {
+      button.addEventListener('click',() => {
+          const productId = button.dataset.productId;
+          addtocart(productId);
+          updateaddtocart();
+      });
+  });
 }
-
-document.querySelectorAll('.js-addtocart').forEach((button) => {
-    button.addEventListener('click',() => {
-        const productId = button.dataset.productId;
-        addtocart(productId);
-        updateaddtocart();
-    });
-});
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
 import { rendorOrderSummary } from "./checkout/orderSummary.js";
 import { rendorPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 
 // import '../data/cart-class.js';
-import '../data/backend-practice.js';
-rendorOrderSummary();
-rendorPaymentSummary();
+// import '../data/backend-practice.js';
+loadProducts(() => {
+    rendorOrderSummary();
+    rendorPaymentSummary();
+});
