5b579d806f76137873c70617fb7f22ac4db39587
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-06-02 21:09:37 +0530

    Converted loadProducts from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,17 +1,13 @@
 import { rendorOrderSummary } from "./checkout/orderSummary.js";
 import { rendorPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts,loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
 Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            resolve();
-        })
-    }),
+    loadProductsFetch(),
     new Promise((resolve) => {
         loadCart(() => {
             resolve();
